73b806b265b7feeb6d3c015d3645b6c70b757ffc
MacBook Pro <macbookpro@MacBook-Pro-MacBook-2.local>
2023-11-06 17:02:37 +0200

    create socket connection

--- src/components/MessageComponent/MessageComponent.tsx
+++ src/components/MessageComponent/MessageComponent.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
-
+import { format } from 'date-fns';
 import userIcon from '../../assets/ticket/user.jpeg';
 
 import styles from './MessageComponent.module.css';
 
-const MessageComponent = () => {
-    return (
-        <div className={styles.messageWrapper}>
-            <img src={userIcon} alt='avatar' className={styles.userAvatar} />
-            <div className={styles.messageData}>
-                <p className={styles.userName}>Jon Dow</p>
-                <p className={styles.messageText}>consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue</p>
-
-            </div>
-            <p className={styles.date}>March 20, 2023 10:06</p>
-        </div>
-    )
+interface IMessage {
+  content: string;
+  id: number;
+  room: number;
+  timestamp: string;
+  user: number;
+  avatar?: string;
+  key: number;
 }
+
+const MessageComponent = ({ content, avatar, timestamp }: IMessage) => {
+  const handleChangeDate = (data: string) => {
+    const parsedDate = new Date(data);
+    return format(parsedDate, 'MMMM d, yyyy HH:mm');
+  };
+
+  return (
+    <div className={styles.messageWrapper}>
+      <img
+        src={avatar || userIcon}
+        alt='avatar'
+        className={styles.userAvatar}
+      />
+      <div className={styles.messageData}>
+        <p className={styles.userName}>{'Jon Dow'}</p>
+        <p className={styles.messageText}>{content}</p>
+      </div>
+      <p className={styles.date}>
+        {timestamp && handleChangeDate(timestamp)}
+      </p>
+    </div>
+  );
+};
 
 export default MessageComponent;
