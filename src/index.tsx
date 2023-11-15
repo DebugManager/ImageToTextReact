23f3824f449240e14bdc34f1f5cfe59df8c75f34
MacBook Pro <macbookpro@MacBook-Pro-MacBook-2.local>
2023-11-15 13:04:40 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,11 @@ import { loadStripe } from '@stripe/stripe-js';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
-const stripePromise = loadStripe('pk_test_51O7yDQDV4Z1ssWPD2djwQ2hHTHlW123md4BuJayXc4U57yFX5fWGyV8v3u8jN0otD88oExiqRjcbKlSmLTB1LerY000I4uwmVV');
+const stripePromise = loadStripe(
+  'pk_test_51O7yDQDV4Z1ssWPD2djwQ2hHTHlW123md4BuJayXc4U57yFX5fWGyV8v3u8jN0otD88oExiqRjcbKlSmLTB1LerY000I4uwmVV'
+);
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
