bffa052cbb3ac3a483fe441e1c2f95024b37adab
MacBook Pro <macbookpro@MacBook-Pro-MacBook-2.local>
2023-11-15 14:21:48 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -23,8 +23,6 @@ const stripePromise = loadStripe(
   'pk_test_51O7yDQDV4Z1ssWPD2djwQ2hHTHlW123md4BuJayXc4U57yFX5fWGyV8v3u8jN0otD88oExiqRjcbKlSmLTB1LerY000I4uwmVV'
 );
 
-
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -39,7 +37,14 @@ root.render(
   </React.StrictMode>
 );
 
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
